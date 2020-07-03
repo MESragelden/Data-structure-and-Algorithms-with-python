e5f6bce9ef1a618bb16b36cf58eede7f5fb435c3
MESragelden <31871280+MESragelden@users.noreply.github.com>
2020-07-03 14:20:37 +0200

    Update 2.1 Binary Search.py

--- 2.1 Binary Search.py
+++ 2.1 Binary Search.py
@@ -1,34 +1,31 @@
-"""You're going to write a binary search function.
-You should use an iterative approach - meaning
-using loops.
-Your function should take two inputs:
-a Python list to search through, and the value
-you're searching for.
-Assume the list only has distinct elements,
-meaning there are no repeated values, and 
-elements are in a strictly increasing order.
-Return the index of value, or -1 if the value
-doesn't exist in the list."""
+def partition(arr,low,high): 
+	i,j = low+1,high # pivot is the first element
+	while(True):
+		while arr[i]<=arr[low]:
+			if i == high:
+				break
+			i+=1
+		while arr[low]<= arr[j]:
+			if j==low:
+				break
+			j-=1
+		if i>=j:
+			break
+		arr[i],arr[j]=arr[j],arr[i]
+	arr[low],arr[j] = arr[j],arr[low]
+	return j
 
-def binary_search(input_array, value):
-    """Your code goes here."""
-    st = 0
-    end = len(input_array)-1
-    while st != end:
-        mid = st + (end-st) //2
-        if input_array[mid] == value:
-            return mid
-        elif value > input_array[mid]:
-            st = mid+1
-        else:
-            end = mid-1
-    if  input_array[st] == value:
-        return st 
-    else :
-        return -1     
+ 
+def quickSort(arr,low,high): 
+	if low < high: 
+		pi = partition(arr,low,high) 
+		quickSort(arr, low, pi-1) 
+		quickSort(arr, pi+1, high) 
+		return arr
 
-test_list = [1,3,9,11,15,19,29]
-test_val1 = 25
-test_val2 = 15
-print (binary_search(test_list, test_val1))
-print (binary_search(test_list, test_val2))
+def quicksort(arr):
+	return quickSort(arr,0,len(arr)-1)
+ 
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print (quicksort(test))
